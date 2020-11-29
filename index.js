35558e396bc475ff33dd0100f18df885d574b3fd
Мирослав Левдиков <48405824+CyberInitiative@users.noreply.github.com>
2020-11-29 19:29:32 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let section = document.querySelector('.section'); 
+let X = null;
+
+
+
+function MouseDown(event){
+    if(X != null)
+    {
+        console.log(event.clientX);
+        function MouseMOVE(event)
+        {
+
+        }
+
+        function MouseUP(event)
+        {
+
+        }
+
+    }
+}
+
+section.addEventListener("click", MouseDown());
+

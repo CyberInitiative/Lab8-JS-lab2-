bb54c9d59df4925004255279176d28731ef52f94
Мирослав Левдиков <48405824+CyberInitiative@users.noreply.github.com>
2020-12-06 20:02:02 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 let section = document.querySelector('.section'); 
+let nav = document.querySelector('.nav');
+let btn = document.querySelector('.btn'); 
 let X = null;
+let M = 0;
 
 
 
-function MouseDown(event){
-    if(X != null)
+section.addEventListener("mousedown", function(el)
+{
+    X = el.clientX;
+
+
+    addEventListener("mousemove", MouseMove);
+
+    function MouseMove(el)
     {
-        console.log(event.clientX);
-        function MouseMOVE(event)
+        M = el.clientX - X;
+        if(M > 0 && M < 200)
         {
-
+            nav.style.left = -100 + el.clientX / 5+ "px";
+        }else if(M < 0)
+        {
+            console.log(-1);
         }
 
-        function MouseUP(event)
-        {
-
-        }
-
+        addEventListener("mouseup", MouseUp); 
     }
-}
 
-section.addEventListener("click", MouseDown());
+    function MouseUp(el)
+    {
+        if(M > 100){
+            nav.style.left = 0 + "px" 
+        }else
+        {
+            nav.style.left = -100 + "px" 
+        }
+        X = null;
 
+        removeEventListener("mousemove", MouseMove);
+    }
+})
